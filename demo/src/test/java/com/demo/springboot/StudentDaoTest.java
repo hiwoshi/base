5140c1a79bc2a3d6117eed966171ab6edaa9db0c
hiwoshi <1564966546@qq.com>
2019-08-07 17:23:02 +0800

    添加springboot Tets

---
+++ demo/src/test/java/com/demo/springboot/StudentDaoTest.java
@@ -0,0 +1,29 @@
+package com.demo.springboot;
+
+import com.alibaba.fastjson.JSON;
+import com.demo.dao.StudentDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author : shenhao
+ * @date : 2019/8/7 16:44
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentDaoTest {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    @Test
+    public void test(){
+        String s = JSON.toJSONString(studentDao.getStudents());
+        System.out.println(s);
+    }
+
+
+}
